c29b67eb4b82ec1322ed1fa7814becbeadfb5728
Francis Monwe <monwefrancis@live.com>
2024-02-09 02:35:17 -0500

    add product assets to appropriate directories

--- frontend/src/Components/Assets/products.js
+++ frontend/src/Components/Assets/products.js
@@ -1,6 +1,11 @@
-import blackshark1 from "./razer_blackshark_v2_pro.png";
-import g_pro_superlight1 from "./";
-import g_pro_wireless1 from "./";
+import blackshark_1 from "./razer-blackshark-v2-pro.png";
+import g_pro_superlight_1 from "./logitech-g-pro-x-superlight.png";
+import g_pro_wireless_1 from "./logitech-g-pro-x-wireless.png";
+import ps5_1 from "./playstation-5.png";
+import seriesx_1 from "./xbox-series-x.png";
+import cloud3_wireless_1 from "./hyperx-cloud-3-wireless.png";
+import viper_v2_pro_1 from "./razer-viper-v2-pro.png";
+import corsair_k100_1 from "./corsair-k100-rgb.png";
 
 let products = [
   {
@@ -8,7 +13,7 @@ let products = [
     name: "Razer Blackshark V2 Pro",
     brand: "Razer",
     category: "headset",
-    image: [blackshark1],
+    image: [blackshark_1],
     price: 200,
   },
   {
@@ -31,7 +36,7 @@ let products = [
     id: 4,
     name: "PlayStation 5",
     brand: "PlayStation",
-    category: "Console",
+    category: "console",
     image: [ps5_1],
     price: 200,
   },
@@ -39,8 +44,32 @@ let products = [
     id: 5,
     name: "Xbox Series X",
     brand: "Xbox",
-    category: "Console",
+    category: "console",
     image: [seriesx_1],
+    price: 200,
+  },
+  {
+    id: 6,
+    name: "HyperX Cloud III Wireless",
+    brand: "HyperX",
+    category: "headset",
+    image: [cloud3_wireless_1],
+    price: 200,
+  },
+  {
+    id: 7,
+    name: "Razer Viper V2 Pro",
+    brand: "Razer",
+    category: "mouse",
+    image: [viper_v2_pro_1],
+    price: 200,
+  },
+  {
+    id: 8,
+    name: "Corsair K100 RGB Optical-Mechanical Gaming Keyboard",
+    brand: "Corsair",
+    category: "keyboard",
+    image: [corsair_k100_1],
     price: 200,
   },
 ];
