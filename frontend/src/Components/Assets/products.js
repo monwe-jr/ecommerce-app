c1947b0c1b0fdcf67b97c1059d9a306179a0f43a
Francis Monwe <monwefrancis@live.com>
2024-02-09 01:12:05 -0500

    Add Component files and resource folders

---
+++ frontend/src/Components/Assets/products.js
@@ -0,0 +1,46 @@
+import blackshark1 from "./razer_blackshark_v2_pro.png";
+import g_pro_superlight1 from "./";
+import g_pro_wireless1 from "./";
+
+let products = [
+  {
+    id: 1,
+    name: "Razer Blackshark V2 Pro",
+    brand: "Razer",
+    category: "headset",
+    image: [blackshark1],
+    price: 200,
+  },
+  {
+    id: 2,
+    name: "Logitech G Pro X Superlight",
+    brand: "Logitech",
+    category: "mouse",
+    image: [g_pro_superlight_1],
+    price: 200,
+  },
+  {
+    id: 3,
+    name: "Logitech G Pro X Wireless",
+    brand: "Logitech",
+    category: "headset",
+    image: [g_pro_wireless_1],
+    price: 200,
+  },
+  {
+    id: 4,
+    name: "PlayStation 5",
+    brand: "PlayStation",
+    category: "Console",
+    image: [ps5_1],
+    price: 200,
+  },
+  {
+    id: 5,
+    name: "Xbox Series X",
+    brand: "Xbox",
+    category: "Console",
+    image: [seriesx_1],
+    price: 200,
+  },
+];
