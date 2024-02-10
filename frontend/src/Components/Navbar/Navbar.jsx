f243dfced8245da70a73fa2b383a545952d691a3
Francis Monwe <monwefrancis@live.com>
2024-02-10 02:20:22 -0500

    update logo picture names and dependencies for cart icon

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import "./Navbar.css";
 import logo from "../Assets/logo-1.png";
-import cart_icon from "../Assets/cart-icon.png";
+import { FaShoppingCart } from "react-icons/fa";
 
 export const Navbar = () => {
   return (
-    <div className="navBar">
-      <div className="navLogo">
-        <img src={logo} alt=""></img>
-        <p>GameGear</p>
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="" className="logo" />
+        <p>GAMEGEAR</p>
+      </div>
+      <ul className="nav-menu">
+        <li>Shop</li>
+        <li>Headsets</li>
+        <li>Keyboard</li>
+        <li>Mice</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <FaShoppingCart size={35} />
       </div>
     </div>
   );
