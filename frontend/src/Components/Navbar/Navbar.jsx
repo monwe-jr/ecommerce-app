3068e4a63fa3dba6e1b323b7492e6bf7d15db69c
Francis Monwe <monwefrancis@live.com>
2024-02-09 23:03:56 -0500

    Replace logos in Asset folder

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import "./Navbar.css";
+import logo from "../Assets/logo-1.png";
+import cart_icon from "../Assets/cart-icon.png";
 
 export const Navbar = () => {
-  return <div className="navBar">Navbar</div>;
+  return (
+    <div className="navBar">
+      <div className="navLogo">
+        <img src={logo} alt=""></img>
+        <p>GameGear</p>
+      </div>
+    </div>
+  );
 };
