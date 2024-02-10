08898f420811a5b8db337ef92c44b8822bffcef6
Francis Monwe <monwefrancis@live.com>
2024-02-10 17:42:25 -0500

    Add styling to navbar and create pages

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import "./Navbar.css";
 import logo from "../Assets/logo-1.png";
 import { FaShoppingCart } from "react-icons/fa";
+import { useState } from "react";
 
 export const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
+
   return (
     <div className="navbar">
       <div className="nav-logo">
@@ -11,14 +14,39 @@ export const Navbar = () => {
         <p>GAMEGEAR</p>
       </div>
       <ul className="nav-menu">
-        <li>Shop</li>
-        <li>Headsets</li>
-        <li>Keyboard</li>
-        <li>Mice</li>
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          Shop {menu === "shop" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("headsets");
+          }}
+        >
+          Headsets {menu === "headsets" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("keyboards");
+          }}
+        >
+          Keyboards {menu === "keyboards" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("mice");
+          }}
+        >
+          Mice {menu === "mice" ? <hr /> : <></>}
+        </li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
         <FaShoppingCart size={35} />
+        <div className="nav-cart-count">0</div>
       </div>
     </div>
   );
