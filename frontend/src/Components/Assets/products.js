e0832b398f7aef7023167e5f383d9a8558e78a4b
Francis Monwe <monwefrancis@live.com>
2024-02-11 05:12:31 -0500

    Add html structure and css styling for store items

--- frontend/src/Components/Assets/products.js
+++ frontend/src/Components/Assets/products.js
@@ -1,8 +1,6 @@
 import blackshark_1 from "./razer-blackshark-v2-pro.png";
 import g_pro_superlight_1 from "./logitech-g-pro-x-superlight.png";
 import g_pro_wireless_1 from "./logitech-g-pro-x-wireless.png";
-import ps5_1 from "./playstation-5.png";
-import seriesx_1 from "./xbox-series-x.png";
 import cloud3_wireless_1 from "./hyperx-cloud-3-wireless.png";
 import viper_v2_pro_1 from "./razer-viper-v2-pro.png";
 import corsair_k100_1 from "./corsair-k100-rgb.png";
@@ -34,22 +32,6 @@ let products = [
   },
   {
     id: 4,
-    name: "PlayStation 5",
-    brand: "PlayStation",
-    category: "console",
-    image: [ps5_1],
-    price: 200,
-  },
-  {
-    id: 5,
-    name: "Xbox Series X",
-    brand: "Xbox",
-    category: "console",
-    image: [seriesx_1],
-    price: 200,
-  },
-  {
-    id: 6,
     name: "HyperX Cloud III Wireless",
     brand: "HyperX",
     category: "headset",
@@ -57,7 +39,7 @@ let products = [
     price: 200,
   },
   {
-    id: 7,
+    id: 5,
     name: "Razer Viper V2 Pro",
     brand: "Razer",
     category: "mouse",
@@ -65,7 +47,7 @@ let products = [
     price: 200,
   },
   {
-    id: 8,
+    id: 6,
     name: "Corsair K100 RGB Optical-Mechanical Gaming Keyboard",
     brand: "Corsair",
     category: "keyboard",
--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import "./Item.css";
 
 export const Item = () => {
-  return <div>Item</div>;
+  return (
+    <div className="item">
+      <img src={props.image} alt="" />
+      <p>{props.name}</p>
+      <div className="item-prices">
+        <div className="item-price-new">{props.new_price}</div>
+        <div className="item-price-old">{props.old_price}</div>
+      </div>
+    </div>
+  );
 };
