f3b039f781edfa254d06cce1f583790063f20e80
Francis Monwe <monwefrancis@live.com>
2024-02-11 02:29:47 -0500

    Create folder and files for new arrival banner and add styling appropriately

---
+++ frontend/src/Components/Hero/Hero.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Hero.css";
+import { FaArrowRight } from "react-icons/fa";
+import hand_icon from "../Assets/hand-icon.png";
+import hero_image from "../Assets/new-arrival.png";
+
+export const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-left">
+        <h2>NEW ARRIVALS ONLY</h2>
+        <div>
+          <div className="hero-hand-icon">
+            <p>new</p>
+            <img src={hand_icon} alt="" />
+          </div>
+          <p>collections</p>
+          <p>for everyone</p>
+        </div>
+        <div className="hero-latest-button">
+          <div>Latest Collection</div>
+          <FaArrowRight />
+        </div>
+      </div>
+      <div className="hero-right">
+        <img src={hero_image} alt="" />
+      </div>
+    </div>
+  );
+};
