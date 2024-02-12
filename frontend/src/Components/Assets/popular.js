9cffa348f7898a3926459c8ffa16fdc0cef6d132
Francis Monwe <monwefrancis@live.com>
2024-02-12 00:23:18 -0500

    Create file for popular products

---
+++ frontend/src/Components/Assets/popular.js
@@ -0,0 +1,39 @@
+import viper_v2_pro_1 from "./razer-viper-v2-pro.png";
+import deathadder_v3_pro_1 from "./razer-deathadder-v3-pro.png";
+import g_pro_superlight_1 from "./logitech-g-pro-x-superlight.png";
+import cloud3_wireless_1 from "./hyperx-cloud-3-wireless.png";
+
+let popular = [
+  {
+    id: 1,
+    name: "Logitech G Pro X Superlight",
+    brand: "Logitech",
+    category: "mouse",
+    image: [g_pro_superlight_1],
+    price: 200,
+  },
+  {
+    id: 2,
+    name: "Razer Deathadder V3 Pro",
+    brand: "Razer",
+    category: "mouse",
+    image: [deathadder_v3_pro_1],
+    price: 200,
+  },
+  {
+    id: 3,
+    name: "Razer Viper V2 Pro",
+    brand: "Razer",
+    category: "mouse",
+    image: [viper_v2_pro_1],
+    price: 200,
+  },
+  {
+    id: 4,
+    name: "HyperX Cloud III Wireless",
+    brand: "HyperX",
+    category: "headset",
+    image: [cloud3_wireless_1],
+    price: 200,
+  },
+];
