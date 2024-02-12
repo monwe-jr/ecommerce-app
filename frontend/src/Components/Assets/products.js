2c9b8da0b54d92444452a3393925475a856ef3d1
Francis Monwe <monwefrancis@live.com>
2024-02-12 00:00:23 -0500

    Add additional store assets

--- frontend/src/Components/Assets/products.js
+++ frontend/src/Components/Assets/products.js
@@ -1,17 +1,32 @@
 import blackshark_1 from "./razer-blackshark-v2-pro.png";
-import g_pro_superlight_1 from "./logitech-g-pro-x-superlight.png";
-import g_pro_wireless_1 from "./logitech-g-pro-x-wireless.png";
-import cloud3_wireless_1 from "./hyperx-cloud-3-wireless.png";
 import viper_v2_pro_1 from "./razer-viper-v2-pro.png";
+import barracuda_x_plus_1 from "./razer-barracuda-x-plus.png";
+import deathadder_v3_pro_1 from "./razer-deathadder-v3-pro.png";
+import huntsman_mini_1 from "./razer-huntsman-mini.png";
+import g_pro_superlight_1 from "./logitech-g-pro-x-superlight.png";
+import g_pro_wireless_1 from "./logitech-g-pro-x-lightspeed.png";
+import logitech_g305_wireless_1 from "./logitech-g305-wireless.png";
+import logitech_g432_1 from "./logitech-g432-gaming-headset.png";
+import logitech_g502_wireless_1 from "./logitech-g502-lightspeed.png";
+import logitech_g915_tkl_wireless_1 from "./logitech-g915-tkl-lightspeed.png";
+import cloud3_wireless_1 from "./hyperx-cloud-3-wireless.png";
+import pulsefire_haste_1 from "./hyperx-pulsefire-haste.png";
 import corsair_k100_1 from "./corsair-k100-rgb.png";
+import corsair_k60_1 from "./corsair-k60-pro.png";
+import corsair_hs55_1 from "./corsair-hs55-wireless.png";
+import corsair_k70_1 from "./corsair-k70-bluetooth.png";
+import astro_a50_1 from "./astro-gaming-a50.png";
+import ducky_3_1 from "./ducky-one-3.png";
+import k530_pro_draconic_1 from "./redragon-k530-pro-draconic.png";
+import arctis1_1 from "./steelseries-arctis-1.png";
 
 let products = [
   {
     id: 1,
-    name: "Razer Blackshark V2 Pro",
+    name: "Razer Deathadder V3 Pro",
     brand: "Razer",
-    category: "headset",
-    image: [blackshark_1],
+    category: "mouse",
+    image: [deathadder_v3_pro_1],
     price: 200,
   },
   {
@@ -24,10 +39,10 @@ let products = [
   },
   {
     id: 3,
-    name: "Logitech G Pro X Wireless",
-    brand: "Logitech",
-    category: "headset",
-    image: [g_pro_wireless_1],
+    name: "Razer Viper V2 Pro",
+    brand: "Razer",
+    category: "mouse",
+    image: [viper_v2_pro_1],
     price: 200,
   },
   {
@@ -40,18 +55,138 @@ let products = [
   },
   {
     id: 5,
-    name: "Razer Viper V2 Pro",
-    brand: "Razer",
-    category: "mouse",
-    image: [viper_v2_pro_1],
+    name: "Logitech G Pro X Lightspeed",
+    brand: "Logitech",
+    category: "headset",
+    image: [g_pro_wireless_1],
     price: 200,
   },
   {
     id: 6,
+    name: "Corsair HS55 Wireless",
+    brand: "Corsair",
+    category: "headset",
+    image: [corsair_hs55_1],
+    price: 200,
+  },
+  {
+    id: 7,
+    name: "Razer Barracuda X Plus",
+    brand: "Razer",
+    category: "headset",
+    image: [barracuda_x_plus_1],
+    price: 200,
+  },
+  {
+    id: 8,
+    name: "Razer Barracuda X Plus",
+    brand: "Razer",
+    category: "headset",
+    image: [astro_a50_1],
+    price: 200,
+  },
+  {
+    id: 9,
+    name: "Astro Gaming A50",
+    brand: "Astro",
+    category: "headset",
+    image: [huntsman_mini_1],
+    price: 200,
+  },
+  {
+    id: 10,
+    name: "Corsair K70 Mini",
+    brand: "Corsair",
+    category: "keyboard",
+    image: [corsair_k70_1],
+    price: 200,
+  },
+  {
+    id: 11,
+    name: "Razer Blackshark V2 Pro",
+    brand: "Razer",
+    category: "headset",
+    image: [blackshark_1],
+    price: 200,
+  },
+  {
+    id: 12,
+    name: "Logitech G305 Wireless",
+    brand: "Logitech",
+    category: "mouse",
+    image: [logitech_g305_wireless_1],
+    price: 200,
+  },
+  {
+    id: 13,
+    name: "logitech G432",
+    brand: "Logitech",
+    category: "headset",
+    image: [logitech_g432_1],
+    price: 200,
+  },
+  {
+    id: 14,
+    name: "Redragon K530 Pro Draconic",
+    brand: "Redragon",
+    category: "keyboard",
+    image: [k530_pro_draconic_1],
+    price: 200,
+  },
+  {
+    id: 15,
+    name: "Logitech G502 Lightspeed",
+    brand: "Logitech",
+    category: "mouse",
+    image: [logitech_g502_wireless_1],
+    price: 200,
+  },
+  {
+    id: 16,
+    name: "Ducky One 3",
+    brand: "Ducky",
+    category: "keyboard",
+    image: [ducky_3_1],
+    price: 200,
+  },
+  {
+    id: 17,
+    name: "Steelseries Arctis 1",
+    brand: "Steelseries",
+    category: "headset",
+    image: [arctis1_1],
+    price: 200,
+  },
+  {
+    id: 18,
+    name: "Logitech G915 TKL Lightspeed",
+    brand: "Logitech",
+    category: "keyboard",
+    image: [logitech_g915_tkl_wireless_1],
+    price: 200,
+  },
+  {
+    id: 19,
     name: "Corsair K100 RGB Optical-Mechanical Gaming Keyboard",
     brand: "Corsair",
     category: "keyboard",
     image: [corsair_k100_1],
+    price: 200,
+  },
+  {
+    id: 20,
+    name: "HyperX Pulsefire Haste",
+    brand: "HyperX",
+    category: "mouse",
+    image: [pulsefire_haste_1],
+    price: 200,
+  },
+  {
+    id: 21,
+    name: "Corsair K60 Pro",
+    brand: "Corsair",
+    category: "keyboard",
+    image: [corsair_k60_1],
     price: 200,
   },
 ];
