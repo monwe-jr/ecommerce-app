b7866b017e59b976095bf71e10a9168b65bad692
Francis Monwe <monwefrancis@live.com>
2024-02-12 04:41:54 -0500

    Add best sellers component and styling

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import "./Item.css";
 
-export const Item = () => {
+export const Item = (props) => {
   return (
     <div className="item">
       <img src={props.image} alt="" />
       <p>{props.name}</p>
       <div className="item-prices">
-        <div className="item-price-new">{props.new_price}</div>
-        <div className="item-price-old">{props.old_price}</div>
+        <div className="item-price">
+          {props.new_price !== null ? `$${props.new_price}` : null}
+        </div>
+        <div
+          className={
+            props.new_price !== null
+              ? "item-price with-line-through"
+              : "item-price"
+          }
+        >
+          ${props.old_price}
+        </div>
       </div>
     </div>
   );
---
+++ frontend/src/Components/Offers/Offers.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Offers.css";
+
+export const Offers = () => {
+  return (
+    <div className="offers">
+      <div className="offers-left"> </div> <div className="offers-right"></div>
+    </div>
+  );
+};
