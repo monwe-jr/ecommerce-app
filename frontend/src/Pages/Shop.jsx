c9626bd360f7c8acded0c7801ab723069c49ac44
Francis Monwe <monwefrancis@live.com>
2024-02-13 01:10:59 -0500

    Add NewsLetter component and styling

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -3,6 +3,7 @@ import { Hero } from "../Components/Hero/Hero";
 import { Popular } from "../Components/Popular/Popular";
 import { Offers } from "../Components/Offers/Offers";
 import { NewArrivals } from "../Components/NewArrivals/NewArrivals";
+import { NewsLetter } from "../Components/NewsLetter/NewsLetter";
 
 export const Shop = () => {
   return (
@@ -11,6 +12,7 @@ export const Shop = () => {
       <Popular />
       <Offers />
       <NewArrivals />
+      <NewsLetter />
     </div>
   );
 };
