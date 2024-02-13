14dcb221701d58c70877c1f0f2b89f7f18edba00
Francis Monwe <monwefrancis@live.com>
2024-02-13 18:16:30 -0500

    Create login page html structure and css file

--- frontend/src/Pages/LoginRegister.jsx
+++ frontend/src/Pages/LoginRegister.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import "./CSS/LoginRegister.css";
 
 export const LoginRegister = () => {
-  return <div>LoginRegister</div>;
+  return (
+    <div className="login-register">
+      <div className="login-register-container">
+        <h1>Sign Up</h1>
+        <div className="login-register-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p className="login-register-login">
+          Already have an account? <span>Login here</span>
+        </p>
+      </div>
+    </div>
+  );
 };
