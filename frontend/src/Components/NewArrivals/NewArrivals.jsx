2afb1d46ea169e8222012560c5c91b430dc15e20
Francis Monwe <monwefrancis@live.com>
2024-02-12 20:57:45 -0500

    Add New Arrivals to shop and fix styling in offer.css

--- frontend/src/Components/NewArrivals/NewArrivals.jsx
+++ frontend/src/Components/NewArrivals/NewArrivals.jsx
@@ -1,6 +1,27 @@
 import React from "react";
 import "./NewArrivals.css";
+import new_arrivals from "../Assets/new";
+import { Item } from "../Item/Item";
 
 export const NewArrivals = () => {
-  return <div>NewArrivals</div>;
+  return (
+    <div className="new-arrivals">
+      <h1>NEW ARRIVALS</h1>
+      <hr />
+      <div className="arrivals">
+        {new_arrivals.map((item, i) => {
+          return (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
