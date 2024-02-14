02586b3851b67b646834fc3f8ddba4dd2bdc7372
Francis Monwe <monwefrancis@live.com>
2024-02-14 00:55:55 -0500

    Add breadcrums component and create productdisplay component files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,7 +31,7 @@ function App() {
             element={<Category banner={mouse_banner} category="mouse" />}
           />
           <Route path="/product" element={<Product />} />
-          <Route path=":productId" element={<Product />} />
+          <Route path="/product/:productId" element={<Product />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginRegister />} />
         </Routes>
