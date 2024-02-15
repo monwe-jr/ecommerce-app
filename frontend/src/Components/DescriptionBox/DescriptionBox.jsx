8338d1a39f581a2e0ea1cdde35b12a98f3e9b7c9
Francis Monwe <monwefrancis@live.com>
2024-02-15 04:23:10 -0500

    Add description box and related product components to the product page

--- frontend/src/Components/DescriptionBox/DescriptionBox.jsx
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -2,5 +2,28 @@ import React from "react";
 import "./DescriptionBox.css";
 
 export const DescriptionBox = () => {
-  return <div className="descriptionbox"></div>;
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Welcome to GAMEGEAR, your ultimate destination for high-quality gaming
+          peripherals. Explore our extensive collection of gaming mice,
+          headsets, and keyboards, meticulously curated to enhance your gaming
+          experience. Whether you're a casual gamer or a competitive enthusiast,
+          we offer cutting-edge technology and ergonomic designs to help you
+          stay ahead of the game. Browse our selection of top brands, including
+          Razer, Logitech, HyperX, and more, and find the perfect peripherals to
+          elevate your gameplay.
+        </p>
+        <p>
+          With fast shipping and exceptional customer service, GAMEGEAR is your
+          go-to destination for all your gaming needs. Start shopping now and
+          level up your gaming setup today!
+        </p>
+      </div>
+    </div>
+  );
 };
---
+++ frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./RelatedProducts.css";
+import related from "../Assets/popular";
+import { Item } from "../Item/Item";
+
+export const RelatedProducts = () => {
+  return (
+    <div className="relatedproducts">
+      <h1>Related Products</h1>
+      <hr />
+      <div className="relatedproducts-item">
+        {related.map((item, i) => {
+          return (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
