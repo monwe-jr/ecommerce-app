0cf13150bacfca037feececcf9ea26f7beab16c1
Francis Monwe <monwefrancis@live.com>
2024-02-16 01:20:26 -0500

    Add cart functionality and html structure for cart page

---
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -0,0 +1,47 @@
+import React, { useContext } from "react";
+import "./CartItems.css";
+import { ShopContext } from "../../Context/ShopContext";
+import remove_icon from "../Assets/cross-icon.png";
+
+export const CartItems = () => {
+  const { products, cartItems, removeFromCart } = useContext(ShopContext);
+
+  return (
+    <div className="cartitems">
+      <div className="cartitems-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Total</p>
+        <p>Remove</p>
+      </div>
+      <hr />
+      {products.map((e) => {
+        if (cartItems[e.id] > 0) {
+          return (
+            <div>
+              <div className="cartitems-format">
+                <img src={e.image} alt="" className="carticon-product-icon" />
+                <p>{e.name}</p>
+                <p>${e.price}</p>
+                <button className="cartitems-quantity">
+                  {cartItems[e.id]}
+                </button>
+                <p>{e.new_price * cartItems[e.id]}</p>
+                <img
+                  src={remove_icon}
+                  onClick={() => {
+                    removeFromCart(e.id);
+                  }}
+                  alt=""
+                />
+              </div>
+              <hr />
+            </div>
+          );
+        }
+      })}
+    </div>
+  );
+};
--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,10 +1,27 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import products from "../Components/Assets/products";
 
 export const ShopContext = createContext(null);
+const getDefaultCart = () => {
+  let cart = {};
+  products.forEach((product) => {
+    cart[product.id] = 0;
+  });
+  return cart;
+};
 
 const ShopContextProvider = (props) => {
-  const contextValue = { products };
+  const [cartItems, setCartItems] = useState(getDefaultCart());
+
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  const contextValue = { products, cartItems, addToCart, removeFromCart };
 
   return (
     <ShopContext.Provider value={contextValue}>
