cd96b2998a7e38ba31a3a5f7a0718f61c329b913
Francis Monwe <monwefrancis@live.com>
2024-02-16 05:28:24 -0500

    Add item count to cart icon in navbar

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -36,7 +36,18 @@ const ShopContextProvider = (props) => {
     return totalAmount;
   };
 
+  const getTotalCartItems = () => {
+    let totalItem = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        totalItem += cartItems[item];
+      }
+    }
+    return totalItem;
+  };
+
   const contextValue = {
+    getTotalCartItems,
     getTotalCartAmount,
     products,
     cartItems,
