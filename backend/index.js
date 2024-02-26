63308e496dda361f51c9845378c905c76d926d83
Francis Monwe <monwefrancis@live.com>
2024-02-25 21:49:42 -0500

    Hide mongoURL

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const port = 4000;
 const express = require("express");
 const app = express();
@@ -11,10 +13,13 @@ app.use(express.json());
 app.use(cors());
 
 // Database with MongoDB
-mongoose.connect(
-  "mongodb+srv://GAMEGEARDEV:Cm7mWqMSyp21HRCx@cluster0.ywstpbi.mongodb.net/e-commerce"
-);
-
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 //API Creation
 app.get("/", (req, res) => {
   res.send("Express App is Running");
